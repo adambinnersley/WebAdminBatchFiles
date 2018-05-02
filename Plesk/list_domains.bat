3d9f6e55341de86605fe7ee29317509642a4d87f
Adam Binnersley <AdamB7586@users.noreply.github.com>
2018-05-02 10:18:58 +0100

    Update for Plesk cli changes

--- Plesk/list_domains.bat
+++ Plesk/list_domains.bat
@@ -4,6 +4,6 @@ set plesk_password = '';
 
 cd "%plesk_cli%"
 
-"%plesk_dir%"\MySQL\bin\mysql.exe -uadmin -p%plesk_password% -P8306 psa -Ne"select name from domains" > c:\all_domains.txt
+plesk db "select name from domains" > c:\all_domains.txt
 
 pause
