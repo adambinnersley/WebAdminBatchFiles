502cdfdb0ef75e77b9171865a85dd77981d1b306
Adam Binnersley <AdamB7586@users.noreply.github.com>
2019-02-18 11:21:16 +0000

    Update PFXCertCreate.ps1
    As the SSL Certificates remain in the certificates store make sure the SSL certificate is valid for a set period of time

--- SmarterMail/PFXCertCreate.ps1
+++ SmarterMail/PFXCertCreate.ps1
@@ -2,5 +2,5 @@ $maildomain = 'mailhost.co.uk';
 $mail_admin_password = 'password123';
 
 Get-ChildItem -Path 'Cert:\localmachine\My' |
-Where-Object {$_.Subject -like "*${maildomain}*"} | 
+Where-Object {$_.Subject -like "*${maildomain}*" -and $_.NotAfter -lt (Get-Date).AddDays(30)} | 
 Foreach-Object {&certutil.exe @('-exportpfx', '-f', '-p', $mail_admin_password, $_.Thumbprint, "C:\SmarterMail\Certificates\mail.${maildomain}.pfx")}
