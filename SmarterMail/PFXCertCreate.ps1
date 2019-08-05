d07cddc01d8bcf1690eba43ebd9f985befb7da47
Adam Binnersley <AdamB7586@users.noreply.github.com>
2019-08-05 15:01:13 +0100

    Update PFXCertCreate.ps1

--- SmarterMail/PFXCertCreate.ps1
+++ SmarterMail/PFXCertCreate.ps1
@@ -1,6 +1,8 @@
 $maildomain = 'mailhost.co.uk';
 $mail_admin_password = 'password123';
 
-Get-ChildItem -Path 'Cert:\localmachine\My' |
-Where-Object {$_.Subject -like "*${maildomain}*" -and $_.NotAfter -lt (Get-Date).AddDays(30)} | 
+(Get-ChildItem -Path 'Cert:\localmachine\My' |
+Where-Object {$_.Subject -like "*${maildomain}*"} | 
+Sort-Object -Property NotAfter -Descending | 
+Select-Object -first 1) | 
 Foreach-Object {&certutil.exe @('-exportpfx', '-f', '-p', $mail_admin_password, $_.Thumbprint, "C:\SmarterMail\Certificates\mail.${maildomain}.pfx")}
