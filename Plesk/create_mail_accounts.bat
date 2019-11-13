0d5da79233618e59b49104562dcbbd7e2c70c243
Adam Binnersley <AdamB7586@users.noreply.github.com>
2019-11-13 11:48:18 +0000

    Update create_mail_accounts.bat

--- Plesk/create_mail_accounts.bat
+++ Plesk/create_mail_accounts.bat
@@ -4,7 +4,7 @@ set file_location = "c:\Users\Administrator\Desktop\domains.txt";
 set defaultmailpassword = "mypassword";
 
 for /f "tokens=*" %%i in (%file_location%) do (
-		"%plesk_bin%\mail.exe" -c info@%%i -passwd %defaultmailpassword% -mailbox true -aliases add:student
+    plesk bin mail.exe -c info@%%i -passwd %defaultmailpassword% -mailbox true -aliases add:student
 )
 
 pause
